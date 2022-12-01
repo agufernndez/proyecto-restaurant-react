145de1980fd91c888855f3442c1fb1abcae63e34
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2022-11-30 21:14:01 -0300

    ultimo

--- frontend/src/components/layout/Header/Header.js
+++ frontend/src/components/layout/Header/Header.js
@@ -1,13 +1,15 @@
-// import logo from "../../../assets/Logo.png"
-// import iconMenu from "../../../assets/icon/bx_menu.svg"
+// import logo from "../../../../public/assets/Logo.png"
+
+// import logo from "/frontend/public/assets/Logo.png" 
+// import iconMenu from "/frontend/public/assets/icon/bx_menu.svg"
 import "./header.css"
 
 const Header = () => {
     return (
         <header className="header">
             <h1 className="h1Header">Hola soy el componente Header</h1>
-            {/* <img src={logo} alt="Logo Food Zero" className="logo"/>
-            <img src={iconMenu} alt="Toggle Menu" className="toggleMenu"/> */}
+            {/* <img src={logo} alt="Logo Food Zero" className="logo"/> */}
+            {/* <img src={iconMenu} alt="Toggle Menu" className="toggleMenu"/> */}
             {/* <!-- ACA PONER EN NAVBAR MOBILE CON LAS SECCIONES DE INICIO - CONTACTANOS - MENU - VINOS & COCTAILS --> */}
             {/* <!-- <nav>
             <ul>
