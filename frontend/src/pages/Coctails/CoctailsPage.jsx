5b50f5ad8d91de1187b03829aacf21af9fb30b8e
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2022-12-01 21:56:45 -0300

    base de CoctailsPage.jsx, le falta contenido en el main y su css

---
+++ frontend/src/pages/Coctails/CoctailsPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../../components/layout/Footer/Footer';
+import Header from '../../components/layout/Header/Header';
+
+const CoctailsPage = () => {
+    return ( 
+        <div>
+            <Header/>
+            
+            <Footer/>
+        </div>
+     );
+}
+ 
+export default CoctailsPage;
