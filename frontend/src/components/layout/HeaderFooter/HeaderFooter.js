b6c81d97e1c8a2082eaf55dca9fe52324c0b6948
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2022-12-24 14:27:34 -0300

    unifique el header con el footer, elimine Navbar, modifique tamaño del logo

---
+++ frontend/src/components/layout/HeaderFooter/HeaderFooter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './headerFooter.css'
+
+const HeaderFooter = () => {
+    return (
+        <div>
+            <footer className='footer'>
+                <img src="./assets/Logo.png" alt="Logo Food Zero" className="logo"/>                
+                <ul>
+                        <li><Link to="/">Inicio</Link></li>
+                        <li><Link to="/menu">Menu</Link></li>
+                        <li><Link to="/contactanos">Contactanos</Link></li>
+                        <li><Link to="/coctails">Vinos & Coctails</Link></li>
+                </ul>
+            </footer>
+        </div>
+
+    )
+}
+
+export default HeaderFooter;
