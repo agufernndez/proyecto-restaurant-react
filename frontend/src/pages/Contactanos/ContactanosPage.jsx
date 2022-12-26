3905fdac832640e6e24f95f6005984e23c1e9282
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2022-12-26 14:59:11 -0300

    modifique la className del button de 'contactanos'

--- frontend/src/pages/Contactanos/ContactanosPage.jsx
+++ frontend/src/pages/Contactanos/ContactanosPage.jsx
@@ -12,7 +12,7 @@ const ContactanosPage = () => {
                         <input type="text" name="nombre" id="nombre" placeholder="Ingrese aquí su nombre"/>
                         <input type="email" name= "email" id="email" placeholder="Ingrese aquí su e-mail"/>
                         <textarea name="consultas" id="consultas" cols="30" rows="10" placeholder="Ingrese quí su consulta" className="textarea"></textarea>
-                        <input type="submit" value="Enviar" className="button"/>
+                        <input type="submit" value="Enviar" className="inputEnviar"/>
                     </form>
 
                 </div>
