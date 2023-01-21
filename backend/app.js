41190d49eed8f5ef796a985942550e813a44d2d2
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2023-01-21 10:37:37 -0300

    inicie proyecto con express, instale dependencias (nodemon, util,express-session, md5, mysql), cree carpeta admin en views con 2 archivos (login y layout), cree un formulario en views/admin/login, cree carpeta models en la raiz, archivo .env, entre otros

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/admin/login');
-
 
 var app = express();
 
@@ -23,8 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/admin/login', loginRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
