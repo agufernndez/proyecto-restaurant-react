acaec2930839df71d14d8154312e1cc50273eb1c
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2023-02-05 17:07:15 -0300

    integrando envio de mail

--- frontend/src/pages/Contactanos/ContactanosPage.jsx
+++ frontend/src/pages/Contactanos/ContactanosPage.jsx
@@ -1,18 +1,62 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 import './contactanos.css'
 
-const ContactanosPage = () => {
+const ContactanosPage = (props) => {
+
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        consulta: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true);
+        const response = await axios.post('http://localhost:3000/api/contactanos', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
     return ( 
         <div>
             <main className="contactanosPage">
                 <div>
                     <h1>Contactanos</h1>
-                    <form action="">
-                        <input type="text" name="nombre" id="nombre" placeholder="Ingrese aquí su nombre"/>
-                        <input type="email" name= "email" id="email" placeholder="Ingrese aquí su e-mail"/>
-                        <textarea name="consultas" id="consultas" cols="30" rows="10" placeholder="Ingrese quí su consulta" className="textarea"></textarea>
+                    <form action="/contactanos" method='post' onSubmit={handleSubmit}>
+
+                        <input type="text" name="nombre" id="nombre" value={formData.nombre} onChange={handleChange} placeholder="Ingrese aquí su nombre"/>
+
+                        <input type="email" name="email" id="email" value={formData.email} onChange={handleChange} placeholder="Ingrese aquí su e-mail"/>
+
+                        <input type="telefono" name="telefono" id="telefono" value={formData.telefono} onChange={handleChange} placeholder="Ingrese aquí su telefono"/>
+
+                        <textarea name="consulta" id="consulta" value={formData.consulta} onChange={handleChange} cols="30" rows="10" placeholder="Ingrese quí su consulta" className="textarea"></textarea>
+                        
                         <input type="submit" value="Enviar" className="inputEnviar"/>
+
+                    {sending ? <p>Enviando..</p> : null }
+                    {msg ? <p>{msg}</p> : null}
+
                     </form>
 
                 </div>
