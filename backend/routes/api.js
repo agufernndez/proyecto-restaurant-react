a0322106a6970366ef79c4b45df073ca0f4a1f21
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2023-02-05 17:09:38 -0300

    integrando con envio de mail

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var menuSemanalModel = require('../models/menuSemanalModels');
 // var util = require('util');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 router.get('/menuSemanal', async function (req, res, next) {
     let menuSemanal = await menuSemanalModel.getMenuSemanal();
@@ -29,4 +30,33 @@ router.get('/menuSemanal', async function (req, res, next) {
     res.json(menuSemanal);
 });
 
-module.exports = router
+router.post('/contactanos', async (req, res) => {
+    const mail = {
+        to: 'lic.fernandezagustina@gmail.com',
+        subject: 'Contacto web',
+        html: `${req.body.nombre} se contacto a través de la web y quiere más información al correo: ${req.body.email} <br> Además realizó la siguiente consulta: ${req.body.consulta} <br> Su número de celular es: ${req.body.telefono}`
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS
+        }
+      });
+
+      await transport.sendMail(mail)
+
+      res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado!'
+      });
+});
+
+// SMTP_HOST=smtp.mailtrap.io
+// SMTP_PORT=2525
+// SMTP_USER=6dca93d8297618
+// SMTP_PASS=a1a691ce5438d9
+
+module.exports = router;
