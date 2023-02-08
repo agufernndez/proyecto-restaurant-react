c1f73abdcc65d3b6e4b80ef5a3228c8348405fa8
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2023-02-07 21:49:13 -0300

    mejorando el diseño del backend

--- frontend/src/components/MenuSemanal/MenuItem.js
+++ frontend/src/components/MenuSemanal/MenuItem.js
@@ -6,7 +6,7 @@ const MenuItem = (props) => {
     const {title, description, imagen, price} = props;
 
     return (
-        <div>
+        <div className='menuSemanal'>
             <h1>{title}</h1>
             <h2>{description}</h2>
             <img src={imagen}/>
